efaa42e04cdb92a9b0fc3d016c0c1ebda37a6165
patrick-zippenfenig <patrick@zippenfenig.de>
2022-08-08 20:51:51 +0200

    cleanup

--- Sources/SwiftEccodes/SwiftEccodes.swift
+++ Sources/SwiftEccodes/SwiftEccodes.swift
@@ -1,85 +1,41 @@
 import CEccodes
 
-public struct SwiftEccodes {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
-
 public enum EccodesError: Error {
     case cannotOpenFile(filename: String, errno: Int32, error: String)
+    case cannotGetData
 }
 
-
-public final class GribMessage {
-    let h: OpaquePointer
-    
-    public init(h: OpaquePointer) {
-        self.h = h
-    }
-    
-    public func getData() -> [Double] {
-        var size = 0
-        guard codes_get_size(h, "values", &size) == 0 else {
-            fatalError()
-        }
-        
-        return [Double](unsafeUninitializedCapacity: size) { buffer, initializedCount in
-            var size = size
-            guard codes_get_double_array(h, "values", buffer.baseAddress, &size) == 0 else {
-                fatalError()
-            }
-            initializedCount += size
-        }
-    }
-    
-    public func get(attribute: String) -> String? {
-        var length = 0
-        guard grib_get_length(h, attribute, &length) == 0 else {
-            return nil
-        }
-        return String(unsafeUninitializedCapacity: length-1) { buffer in
-            guard codes_get_string(h, attribute, buffer.baseAddress, &length) == 0 else {
-                fatalError()
-            }
-            return length-1
-        }
-    }
-    
-    public func iterate(namespace: String) -> AnyIterator<(key: String, value: String)> {
-        guard let kiter  = codes_keys_iterator_new(h, UInt(CODES_KEYS_ITERATOR_ALL_KEYS | CODES_KEYS_ITERATOR_SKIP_DUPLICATES), namespace) else {
-            fatalError()
-        }
-
-        return AnyIterator {
-            guard codes_keys_iterator_next(kiter) == 1 else {
-                codes_keys_iterator_delete(kiter)
-                return nil
-            }
-            guard let attribute = codes_keys_iterator_get_name(kiter) else {
-                return nil
-            }
-            let key = String(cString: attribute)
-            var length = 0
-            guard grib_get_length(self.h, attribute, &length) == 0 else {
-                return nil
-            }
-            let value = String(unsafeUninitializedCapacity: length-1) { buffer in
-                guard codes_keys_iterator_get_string(kiter, buffer.baseAddress, &length) == 0 else {
-                    fatalError()
-                }
-                return length-1
-            }
-            return (key, value)
-        }
-    }
-    
-    deinit {
-        codes_handle_delete(h)
-    }
+public enum EccodesNamespace: String {
+    case ls
+    case parameter
+    case statistics
+    case time
+    case geography
+    case vertial
+    case mars
 }
 
+public final class GribFile {
+    let fn: UnsafeMutablePointer<FILE>
+    
+    public var messages: AnyIterator<GribMessage> {
+        AnyIterator<GribMessage> {
+            var error: Int32 = 0
+            guard let h = codes_handle_new_from_file(nil, self.fn, PRODUCT_GRIB, &error) else {
+                return nil
+            }
+            return GribMessage(h: h)
+        }
+    }
+    
+    public init(file: String) throws {
+        guard let fn = fopen(file, "r") else {
+            let error = String(cString: strerror(errno))
+            throw EccodesError.cannotOpenFile(filename: file, errno: errno, error: error)
+        }
+        self.fn = fn
+    }
+}
 
 public struct GribMemory {
     let ptr: UnsafeRawBufferPointer
@@ -105,25 +61,74 @@ public struct GribMemory {
     }
 }
 
-
-public final class GribFile {
-    let fn: UnsafeMutablePointer<FILE>
+public final class GribMessage {
+    let h: OpaquePointer
     
-    public var messages: AnyIterator<GribMessage> {
-        AnyIterator<GribMessage> {
-            var error: Int32 = 0
-            guard let h = codes_handle_new_from_file(nil, self.fn, PRODUCT_GRIB, &error) else {
-                return nil
+    public init(h: OpaquePointer) {
+        self.h = h
+    }
+    
+    public func getData() throws -> [Double] {
+        var size = 0
+        guard codes_get_size(h, "values", &size) == 0 else {
+            throw EccodesError.cannotGetData
+        }
+        
+        return try [Double](unsafeUninitializedCapacity: size) { buffer, initializedCount in
+            guard codes_get_double_array(h, "values", buffer.baseAddress, &size) == 0 else {
+                throw EccodesError.cannotGetData
             }
-            return GribMessage(h: h)
+            initializedCount += size
         }
     }
     
-    public init(file: String) throws {        
-        guard let fn = fopen(file, "r") else {
-            let error = String(cString: strerror(errno))
-            throw EccodesError.cannotOpenFile(filename: file, errno: errno, error: error)
+    public func get(attribute: String) -> String? {
+        var length = 0
+        guard grib_get_length(h, attribute, &length) == 0 else {
+            return nil
         }
-        self.fn = fn
+        return String(unsafeUninitializedCapacity: length) { buffer in
+            guard codes_get_string(h, attribute, buffer.baseAddress, &length) == 0 else {
+                fatalError()
+            }
+            // ignore \0
+            return length-1
+        }
+    }
+    
+    public func iterate(namespace: EccodesNamespace) -> AnyIterator<(key: String, value: String)> {
+        guard let kiter  = codes_keys_iterator_new(h, UInt(CODES_KEYS_ITERATOR_ALL_KEYS | CODES_KEYS_ITERATOR_SKIP_DUPLICATES), namespace.rawValue) else {
+            fatalError()
+        }
+
+        return AnyIterator {
+            guard codes_keys_iterator_next(kiter) == 1 else {
+                codes_keys_iterator_delete(kiter)
+                return nil
+            }
+            guard let attribute = codes_keys_iterator_get_name(kiter) else {
+                return nil
+            }
+            let key = String(cString: attribute)
+            var length = 0
+            guard grib_get_length(self.h, attribute, &length) == 0 else {
+                return nil
+            }
+            guard length > 0, key != "bitmap" else {
+                return (key, "")
+            }
+            let value = String(unsafeUninitializedCapacity: length) { buffer in
+                guard codes_keys_iterator_get_string(kiter, buffer.baseAddress, &length) == 0 else {
+                    fatalError()
+                }
+                // ignore \0
+                return length-1
+            }
+            return (key, value)
+        }
+    }
+    
+    deinit {
+        codes_handle_delete(h)
     }
 }
--- Tests/SwiftEccodesTests/SwiftEccodesTests.swift
+++ Tests/SwiftEccodesTests/SwiftEccodesTests.swift
@@ -3,42 +3,33 @@ import XCTest
 
 final class SwiftEccodesTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftEccodes().text, "Hello, World!")
-        
         let file = try GribFile(file: "/Users/patrick/Downloads/test.grib")
         for message in file.messages {
-            message.iterate(namespace: "ls").forEach({
+            message.iterate(namespace: .ls).forEach({
                 print($0)
             })
             print(message.get(attribute: "name")!)
-            let data = message.getData()
+            let data = try message.getData()
             print(data[0..<10])
         }
     }
     
     func testExample2() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftEccodes().text, "Hello, World!")
-        
-        
         let data = try Data(contentsOf: URL(fileURLWithPath: "/Users/patrick/Downloads/test.grib"))
         try data.withUnsafeBytes { ptr in
             let file = GribMemory(ptr: ptr)
             for message in file.messages {
-                message.iterate(namespace: "ls").forEach({
+                message.iterate(namespace: .ls).forEach({
+                    print($0)
+                })
+                message.iterate(namespace: .geography).forEach({
                     print($0)
                 })
                 print(message.get(attribute: "name")!)
-                let data = message.getData()
+                let data = try message.getData()
+                print(data.count)
                 print(data[0..<10])
             }
         }
-        
-
     }
 }
