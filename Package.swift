7b7d54ce0a441908b5fa74c627c8e46b00248372
patrick-zippenfenig <patrick@zippenfenig.de>
2022-08-11 16:03:29 +0200

    small correction in apt name: libeccodes-dev

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .systemLibrary(name: "CEccodes", pkgConfig: "eccodes", providers: [.brew(["eccodes"]), .apt(["eccodes-dev"])]),
+        .systemLibrary(name: "CEccodes", pkgConfig: "eccodes", providers: [.brew(["eccodes"]), .apt(["libeccodes-dev"])]),
         .target(
             name: "SwiftEccodes",
             dependencies: ["CEccodes"]),
